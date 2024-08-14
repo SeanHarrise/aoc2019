c2770b1dbdec5cc192b0f3adb8358e70ec42bd05
seanharrise <seanaaharris@virginmedia.com>
2024-08-14 10:21:09 +0100

    start task three

---
+++ aoc2019/src/main/scala/solutions/DayThreeTaskOne.scala
@@ -0,0 +1,9 @@
+package solution
+
+object DayThreeTaskOne{
+  private val filePath = "/Users/seanharris/git/aoc2019/aoc2019/src/main/resources/day3.txt"
+
+  def getFilepath() : String = {
+    return filePath
+  }
+}
